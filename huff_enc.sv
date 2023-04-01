328fd0579b3c19dbc212e351295e5e6c9333b799
Anusha Raghavendra <araghave@andrew.cmu.edu>
2023-03-31 22:27:39 -0400

    some inputs are working

--- huff_enc.sv
+++ huff_enc.sv
@@ -77,7 +77,7 @@ always_ff @(posedge clk) begin
 end
 */
 
-    freq_calc freq_calc_ins(.data_in(data_in), .data_en(data_en), .node(initial_node), .count(unique_char_count));
+    freq_calc freq_calc_ins(.clk(clk), .reset(reset), .data_in(data_in), .data_en(data_en), .node(initial_node), .count(unique_char_count));
     node_sorter node_sorter_ins(.clk(clk), .count(unique_char_count), .node(in_huff_tree[ll][0:`MAX_STRING_LENGTH-1]), .output_node(out_huff_tree_s[0:`MAX_STRING_LENGTH-1]));
     merge_nodes merge_nodes_ins(.min_node(out_huff_tree_s[0]), .second_min_node(out_huff_tree_s[1]), .merged_node(merged_nodes_list));
 
@@ -93,6 +93,8 @@ always_ff @(posedge clk) begin : huffman_enc
             for (int i=0; i < (2*`MAX_STRING_LENGTH); i++) begin
                 encoded_value[i] = 'bz;
                 encoded_mask[i] = 'b0;
+                encoded_value_h[i] = 'bz;
+                encoded_mask_h[i] = 'b0;
                 huff_tree[i].ascii_char = 'bz;
                 huff_tree[i].frequency = 0;
                 huff_tree[i].is_leaf_node = 1'b0;
@@ -136,11 +138,11 @@ always_ff @(posedge clk) begin : huffman_enc
         end
 
         `MERGE : begin  //state=4
-               for (int i=0; i< count; i++) begin
+               for (int i=0; i< unique_char_count; i++) begin
            // in_huff_tree[0][i] = initial_node[i];
             out_huff_tree[ll][i] = out_huff_tree_s[i];
           end
-            in_huff_tree[ll+1][0:`MAX_STRING_LENGTH-1] = {merged_nodes_list,out_huff_tree[ll][2:`MAX_STRING_LENGTH]};
+            in_huff_tree[ll+1][0:`MAX_STRING_LENGTH-1] = {merged_nodes_list, out_huff_tree[ll][2:`MAX_STRING_LENGTH]}; //somehow put this merged list in the end
             count = count - 1;
             if (count == 0) begin
                 state <= `BUILD_TREE;
@@ -152,7 +154,7 @@ always_ff @(posedge clk) begin : huffman_enc
             end
         end
 
-        `BUILD_TREE: begin  //state=5
+        `BUILD_TREE: begin  //state=5   //might be a critical path
         for (int l=0; l< unique_char_count; l++) begin
             //assigning child nodes and leaf node fields 
         //    $display("count:%d\n", unique_char_count);
@@ -169,8 +171,8 @@ always_ff @(posedge clk) begin : huffman_enc
             huff_tree[2*j + 1].left_node = out_huff_tree[l][1].left_node;
             huff_tree[2*j].right_node = out_huff_tree[l][0].right_node;
             huff_tree[2*j + 1].right_node = out_huff_tree[l][1].right_node;
-            huff_tree[2*j + 1].level = j;
-            huff_tree[2*j].level = j;
+        //    huff_tree[2*j + 1].level = j;
+        //    huff_tree[2*j].level = j;
             end
             else if (j==0) begin
             huff_tree[2*j + 1].ascii_char = out_huff_tree[l][0].ascii_char;
@@ -182,9 +184,9 @@ always_ff @(posedge clk) begin : huffman_enc
             
     end
 
-    //assigning parent field for a node if that node is present as a child node wither in left or right node 
-     for (int l=(2*count)-1; l > 1; l--) begin
-        for (int n=1; n< (2*count); n++) begin
+    //assigning parent field for a node if that node is present as a child node either in left or right node 
+     for (int l=(2*unique_char_count)-1; l > 1; l--) begin
+        for (int n=1; n< (2*unique_char_count); n++) begin
             if (huff_tree[n].left_node == huff_tree[l].ascii_char | huff_tree[n].right_node == huff_tree[l].ascii_char) begin
                 huff_tree[l].parent = n;
             end
@@ -192,9 +194,9 @@ always_ff @(posedge clk) begin : huffman_enc
     end
 
     //assigning levels
-    for (int m=1; m < (2*count); m++) begin
+    for (int m=1; m < (2*`MAX_STRING_LENGTH); m++) begin
         if (huff_tree[m].is_leaf_node != 1) begin
-        for (int n=2; n < (2*count); n++) begin
+        for (int n=2; n < (2*`MAX_STRING_LENGTH); n++) begin
             if (huff_tree[n].ascii_char == huff_tree[m].left_node) begin
                 huff_tree[n].level = huff_tree[m].level + 1;
             end
@@ -204,31 +206,45 @@ always_ff @(posedge clk) begin : huffman_enc
         end
         end
     end
+    state <= `ENCODE;
         end
 
         `ENCODE: begin  //state=6
-               for (int n=2; n < (2*count); n++) begin  //1-root node
+               for (int n=2; n < (2*unique_char_count); n++) begin  //1-root node
                 encoded_mask_h[n] = (1'b1 << huff_tree[n].level) -1;
             if (huff_tree[n].parent != 1) begin   
-                encoded_value_h[n] = (n%2==0) ? (encoded_value_h[huff_tree[n].parent] << (huff_tree[n].level-1)): ((encoded_value_h[huff_tree[n].parent] << (huff_tree[n].level-1)) | 1'b1);
+            //    encoded_value_h[n] = (n%2==0) ? (encoded_value_h[huff_tree[n].parent] << (huff_tree[n].level-1)): ((encoded_value_h[huff_tree[n].parent] << (huff_tree[n].level-1)) | 1'b1);
+            encoded_value_h[n] = (n%2==0) ? (encoded_value_h[huff_tree[n].parent] << 1'b1): ((encoded_value_h[huff_tree[n].parent] << 1'b1) | 1'b1);
             end
             else if (huff_tree[n].parent == 1) begin
                encoded_value_h[n][0] = (n%2==0) ? {1'b0}: {1'b1};
             end
         end
+
+        state <= `SEND_OUTPUT;
+
         end
 
      //extract only encodings for unique characters 
     `SEND_OUTPUT: begin     //state=7
         itr = 0;
-        for (int n=0; n< (2*count); n++) begin
+        for (int n=0; n< (2*unique_char_count); n++) begin
             if (huff_tree[n].is_leaf_node == 1) begin
                 encoded_mask[itr] = encoded_mask_h[n];
                 encoded_value[itr] = encoded_value_h[n];
                 character[itr] = huff_tree[n].ascii_char;
                 itr = itr + 1; 
             end
+            $display("Output sent\n");
         end
+
+        if (data_en) begin
+        state <= `INIT;
+        end
+        else begin
+            state <= `SEND_OUTPUT;
+        end
+
         end
 
         default : begin
@@ -245,7 +261,7 @@ end //posedge_clk
 endmodule
 
 
-module freq_calc(input logic [6:0] data_in[`MAX_STRING_LENGTH], input logic data_en, output node_t node[`MAX_STRING_LENGTH], output integer count);
+module freq_calc(input logic clk, input logic reset, input logic [6:0] data_in[`MAX_STRING_LENGTH], input logic data_en, output node_t node[`MAX_STRING_LENGTH], output integer count);
 //integer node[char];   //like an hash node[a] = frequency- not synthesizable 
 
 //node_t node[`MAX_STRING_LENGTH];
@@ -256,8 +272,18 @@ integer ctr;
 
 
 //for now able to calculate frequency properly, but had to delete the extra nodes: FIXME????
-  always_comb begin
+  always_ff @(posedge clk) begin
     //have to initialize entire node array- FIXME? - else it will impact count
+    if (~reset) begin
+    for (int i=0; i< `MAX_STRING_LENGTH; i++) begin
+            node[i].ascii_char = 'b0;
+            node[i].frequency = 'b0;
+            node[i].left_node = 'b0;   //ascii valud of null
+            node[i].right_node = 'b0;
+            node[i].is_leaf_node = 1'b0;
+    end
+    end 
+    else begin
     if (data_en) begin
         node_create = 1;
         ctr = 0;
@@ -266,10 +292,10 @@ integer ctr;
             break1 = 0;
             cnt = 1'b1;
             index = ctr;
-            node[index].ascii_char = 0;
-            node[index].frequency = 0;
-            node[index].left_node = 0;   //ascii valud of null
-            node[index].right_node = 0;
+            // node[index].ascii_char = 0;
+            // node[index].frequency = 0;
+            // node[index].left_node = 0;   //ascii valud of null
+            // node[index].right_node = 0;
             node[index].is_leaf_node = 1'b1;
             //compare against the existing nodes
             if (`DEBUG) begin
@@ -300,6 +326,7 @@ integer ctr;
                 node[index].frequency = cnt;
         end
         node_create = 1'b1;
+    end
     end
   end
 
@@ -391,10 +418,19 @@ node_t temp_node;
             output_node = node;
         end
         for(int j=0; j< count-1; j++)  begin  //if non-zero frequency ??FIXME
-            if (output_node[j].frequency > output_node[j+1].frequency) begin
+            for (int k=j+1; k< count-1; k++) begin
+            if (output_node[j].frequency == output_node[k].frequency) begin
+                 if (output_node[j].ascii_char > output_node[k].ascii_char) begin
+                    temp_node = output_node[j];
+                    output_node[j] = output_node[k];
+                    output_node[k] = temp_node;
+                end
+            end
+            else if (output_node[j].frequency > output_node[k].frequency) begin
                 temp_node = output_node[j];
-                output_node[j] = output_node[j+1];
-                output_node[j+1] = temp_node;
+                output_node[j] = output_node[k];
+                output_node[k] = temp_node;
+            end
             end
     end
  //   sort_done <= 1'b1;
@@ -474,7 +510,7 @@ genvar ll, cnt;
         //initializing 
         for (int i=0; i < (2*`MAX_STRING_LENGTH); i++) begin
             huff_tree[i].ascii_char = 'bz;
-            huff_tree[i].frequency = 0;
+            huff_tree[i].frequency = 0;r
             huff_tree[i].is_leaf_node = 1'b0;
             huff_tree[i].parent = 'b0;
             huff_tree[i].left_node = 'b0;
@@ -562,6 +598,7 @@ genvar ll, cnt;
 endmodule
 */
 
+
 module tb_top;
   logic [6:0] data;
   logic [6:0] data_in[`MAX_STRING_LENGTH];
@@ -584,11 +621,14 @@ module tb_top;
         clk = 0;
         reset = 0;
     //    cycle = 0;
-        #20 reset = 1;
+        #5 reset = 1;
       //  data_in = '{"a","e"," ","a","a"};
       //    input_string = "anusha is a good gir"; //should be able to read an input file with many strings
       //  input_string = "aftergraduation";
-        input_string = "ae aa";
+     //   input_string = "ae aa"; //working
+       // input_string = "after"; //working
+      //  input_string = "aaaaa";     //no encoding for single character
+        input_string = "aft";     //working  
         foreach(input_string[i]) begin
             data_in[i] = {input_string[i]};   //check in waves 
         end
@@ -597,7 +637,7 @@ module tb_top;
         data_en = 1'b0;
         $monitor("Input data: data:%p\n", data);
    //SEE LATER: #100 data_en = 1'b0;
-   #1500;
+   #25;
    $finish;
     end
 
@@ -638,13 +678,13 @@ module tb_top;
         $display("out_huff_tree[%0d]:%p\n\n", j, DUT.out_huff_tree[j]);
     end
         end
-       for (int i=0; i< 2*`MAX_STRING_LENGTH; i++) begin
+       for (int i=0; i< 2*DUT.unique_char_count; i++) begin
       //  if (DUT.huff_tree[i].ascii_char != 1'bz) begin
-            $display("binary_tree:  huff_tree[%0d]:%p\n", i, DUT.huff_tree[i]);   
+            $display("binary_tree:  huff_tree[%0d]:%p, encoded_mask_h[%0d]:%b, encoded_values_h[%0d]:%b\n", i, DUT.huff_tree[i], i, DUT.encoded_mask_h[i], i, DUT.encoded_value_h[i]);   
       //  end
        end
-        for (int i=0; i< count; i++) begin
-       $display("OUTPUT character:%d, encoded mask[%0d]:%b, encoded values[%0d]:%b\n", DUT.character[i], i, DUT.encoded_mask[i], i, DUT.encoded_value[i]);
+        for (int i=0; i< DUT.unique_char_count; i++) begin
+       $display("OUTPUT character:%s, encoded mask[%0d]:%b, encoded values[%0d]:%b\n", DUT.character[i], i, DUT.encoded_mask[i], i, DUT.encoded_value[i]);
         end
     end
 endmodule 
