f8331524fef5d0de4b83f0cc9a476b9204196c2d
Anusha Raghavendra <araghave@andrew.cmu.edu>
2023-04-23 11:37:50 -0400

    vector read added in python

--- huff_test.py
+++ huff_test.py
@@ -44,51 +44,67 @@ def printNodes(node, val=''):
 		# if node is edge node then
 		# display its huffman code
 	if(not node.left and not node.right):
-		print(f"{node.symbol} -> {newVal}")
+		#print(f"{node.symbol} -> {newVal}")
+
+		
+
 		char_b = bin(ord(node.symbol))
-		print(char_b)
+		char_b = char_b[2:]
+		print('00010'+char_b)
+
 		mask_length = len(newVal)
 		mask = bin((1<<mask_length) - 1)
 		mask = mask[2:]
-		blen = 3
-		blen1 = '{'+'0:0{}b'.format(blen)+'}'
-		print(mask, blen1) 
-		print(blen1.format(int(mask,2)))
-	#	print(mask+newVal)
+		char_len = 3
+		char_len1 = '{'+'0:0{}b'.format(char_len)+'}' 
+		final_mask = char_len1.format(int(mask,2))
+		final_val = char_len1.format(int(newVal,2))
 
-string = "anm"  #read from a vector
-chars = list(string)
+		print('000100'+final_mask+final_val)
 
-# characters for huffman tree
-#chars = ['a', 'n', 'm']
+vector_num = 0
+#can only input characters between a and 0 (hex61 to hex6F)
+#string = "anm"  #read from a input vector
+with open('input_vector.txt', 'r') as f:
+	for line in f:
+		vector_num +=1
+		string, *freq = line.strip().split(',')
+		freq = [int(value) for value in freq]
+		chars = list(string)
+		# characters for huffman tree
+		#chars = ['a', 'n', 'm']
 
-# frequency of characters
-freq = [4, 2, 3]
+		# frequency of characters
+		#freq = [4, 2, 2]
+		#print(string, freq)
+		print("//character")
+		print("//{mask, encoded_value}")
 
 # list containing unused nodes
-nodes = []
+		nodes = []
 
 # converting characters and frequencies
 # into huffman tree nodes
-for x in range(len(chars)):
-	heapq.heappush(nodes, node(freq[x], chars[x]))
+		for x in range(len(chars)):
+			heapq.heappush(nodes, node(freq[x], chars[x]))
 
-while len(nodes) > 1:
+		while len(nodes) > 1:
 
 	# sort all the nodes in ascending order
 	# based on their frequency
-	left = heapq.heappop(nodes)
-	right = heapq.heappop(nodes)
+			left = heapq.heappop(nodes)
+			right = heapq.heappop(nodes)
 
 	# assign directional value to these nodes
-	left.huff = 0
-	right.huff = 1
+			left.huff = 0
+			right.huff = 1
 
 	# combine the 2 smallest nodes to create
 	# new node as their parent
-	newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
+			newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
 
-	heapq.heappush(nodes, newNode)
+			heapq.heappush(nodes, newNode)
 
 # Huffman Tree is ready!
-printNodes(nodes[0])
+		printNodes(nodes[0])
+		print("Vector ", vector_num, " done\n")
